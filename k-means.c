9828b8bceecbd3fd28f2125fd8a60bd65d6adcf6
David Gold <dgold@oberlin.edu>
2018-05-12 13:40:15 -0400

    placing pixels and distance working. All left to do is update centroids.

--- k-means.c
+++ k-means.c
@@ -4,52 +4,72 @@
 #include "k-means.h"
 #include <math.h>
 #include <time.h>
+#include <float.h>
 #define DIMENSIONS 5
 #define CENTROIDS 3
 
 //max 10 pixels
-double  * pixel_data[10];
+//what is being passed from image program
+double  * pixel_raw_data[10];
+struct pixel * pixel_data[10];
 //max 20 pixels can belong to centroid. Should be arraylist.
-double * centroids[CENTROIDS];
-double * centroid_location[CENTROIDS];
-int centroid_array_max_sizes[CENTROIDS];
+struct pixel * centroids[CENTROIDS];
 
 //For testing purposes
 int main(){
 //R G B X Y
 int min_vals[DIMENSIONS]={0,0,0,0,0};
 int max_vals[DIMENSIONS]={252,250,255,1920,1080};
-    srand((unsigned) time(NULL));
+    time_t t;
+    srand((unsigned) time(&t));
     //placing centroids in random initial locations
     for(int i=0;i<CENTROIDS;i++){
-    centroid_location[i] = place_k(min_vals, max_vals);
+	centroids[i]= add_pixel(place_k(min_vals, max_vals));
+	normalize_vals(centroids[i]->location, DIMENSIONS, min_vals, max_vals);
     }
     //setting random values for pixels(as we dont have an image yet) and normalizing said values
     for(int i=0;i<10;i++){
-    pixel_data[i]=place_k(min_vals,max_vals);
-    normalize_vals(pixel_data[i], DIMENSIONS, min_vals, max_vals);
+    pixel_data[i]=add_pixel(place_k(min_vals,max_vals));
+    normalize_vals(pixel_data[i]->location, DIMENSIONS, min_vals, max_vals);
     }
-    double dist=0.0;
-    double comp=0.0;
-    int min;
+    double comp;
+    comp=0;
     for(int i=0;i<10;i++){
 	for(int j=0;j<CENTROIDS;j++){
-	comp=get_distance(pixel_data[i],centroid_location[j]);
-	if(dist>comp){
-	dist=comp; min=j;
+	comp=get_distance(centroids[j]->location, pixel_data[i]->location);
+	if(pixel_data[i]->lowest_dist>comp){
+	pixel_data[i]->lowest_dist=comp; 
+	pixel_data[i]->centroid=j;
 	}
 	}
-	centroid_add(centroids[j], dist);
+    print_pixel(pixel_data[i]);
+    }
+free_all(pixel_data, 10);
+free_all(centroids, CENTROIDS);
+}
+void free_all(struct pixel ** arr, int count){
+    for(int i=0;i<count;i++){
+	free(arr[i]->location);
+	free(arr[i]);
     }
 }
-
-void print_pixel(double * pixel_data, int size){
-    static int pixel=0;
-    printf("Pixel %i: ", pixel++);
-    for(int i=0;i<size;i++){
-	printf("%f ", pixel_data[i]);
+struct pixel * add_pixel(double * location){
+    struct pixel * new=malloc(sizeof(struct pixel));
+    if(!new){
+    perror("Could not allocate space for pixel");
+    exit(4);
     }
-    printf("\n");
+    new->lowest_dist=DBL_MAX;
+    new->centroid=0;
+    new->location=location;
+    return new;
+}
+void print_pixel(struct pixel * p){
+    printf("Location: [");
+    for(int i=0;i<DIMENSIONS;i++){
+	printf("%f,", p->location[i]);
+    }
+    printf("] Centroid: %i Distance: %f \n", p->centroid, p->lowest_dist);
 }
 void normalize_vals(double  *pixel_data, int size, int * min_vals, int * max_vals){
     for(int i=0;i<size; i++){
--- k-means.h
+++ k-means.h
@@ -1,8 +1,15 @@
 #ifndef k_means
 #define k_means
+struct pixel{
+int pixel_num;
+double lowest_dist;
+int centroid;
+double * location;
+};
+void free_all(struct pixel ** arr, int count);
+struct pixel * add_pixel(double * location);
 void normalize_vals(double  *pixel_data, int size, int * min_vals, int * max_vals);
-void print_pixel(double  *pixel_data, int size);
-void print_pixel(double * arr, int size);
+void print_pixel(struct pixel * p);
 double normalize_val(double value, double max_vals, double min_vals);
 double * place_k(int * min_vals, int * max_vals);
 double get_distance(double * centroid_location, double * pixel_data);
