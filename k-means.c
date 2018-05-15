66e633bf57059cbe8a10a1d9fb00bf1854c6c397
David Gold <dgold@oberlin.edu>
2018-05-15 11:52:44 -0400

    removed main method and got rid of globals

--- k-means.c
+++ k-means.c
@@ -5,43 +5,19 @@
 #include <math.h>
 #include <time.h>
 #include <float.h>
-#define DIMENSIONS 5
-#define CENTROIDS 2
-
-//max 10 pixels
-//what is being passed from image program
-double  * pixel_raw_data[6];
-struct pixel * pixel_data[6];
-//max 20 pixels can belong to centroid. Should be arraylist.
+struct pixel ** kmeans(int * min_vals, int * max_vals, int CENTROIDS, int DIMENSIONS, double ** pixel_raw_data){
+//X Y R G B
+pixel_data=malloc(sizeof(int)*NUMPIX);
 struct pixel * centroids[CENTROIDS];
-
-//For testing purposes
-int main(){
-//R G B X Y
-int min_vals[DIMENSIONS]={0,0,0,0,0};
-int max_vals[DIMENSIONS]={252,250,255,3,2};
     time_t t;
     srand((unsigned) time(&t));
     //placing centroids in random initial locations
     for(int i=0;i<CENTROIDS;i++){
-	centroids[i]= add_pixel(place_k(min_vals, max_vals));
+	centroids[i]= add_pixel(place_k(min_vals, max_vals, DIMENSIONS));
 	normalize_vals(centroids[i]->location, DIMENSIONS, min_vals, max_vals);
     }
-    //setting random values for pixels(as we dont have an image yet) and normalizing said values
-    double tmp[DIMENSIONS]={10,10,10,0,0};
-    pixel_data[0]=add_pixel(tmp);
-    double tmp2[DIMENSIONS]={8,11,9,0,1};
-    pixel_data[1]=add_pixel(tmp2);
-    double tmp3[DIMENSIONS]={7,5,2,1,0};
-    pixel_data[2]=add_pixel(tmp3);
-    double tmp4[DIMENSIONS] ={201,200,190,2,1};
-    pixel_data[3]=add_pixel(tmp4);
-    double tmp5[DIMENSIONS]={200,199,198,2,2};
-    pixel_data[4]=add_pixel(tmp5);
-    double tmp7[DIMENSIONS]={212,227,225,3,2};
-    pixel_data[5]=add_pixel(tmp7); 
     for(int i=0;i<NUMPIX;i++){
-	//pixel_data[i]= add_pixel(place_k(min_vals, max_vals));
+	pixel_data[i]=add_pixel(pixel_raw_data[i]);
 	normalize_vals(pixel_data[i]->location, DIMENSIONS, min_vals, max_vals);
     }
     //for how much centroids are moving
@@ -53,7 +29,7 @@ int max_vals[DIMENSIONS]={252,250,255,3,2};
     //gets each pixels centroid distance
     for(int i=0;i<NUMPIX;i++){
 	for(int j=0;j<CENTROIDS;j++){
-	comp=get_distance(centroids[j]->location, pixel_data[i]->location);
+	comp=get_distance(centroids[j]->location, pixel_data[i]->location, DIMENSIONS);
 	if(pixel_data[i]->lowest_dist>comp){
 	pixel_data[i]->lowest_dist=comp; 
 	pixel_data[i]->centroid=j;
@@ -63,19 +39,16 @@ int max_vals[DIMENSIONS]={252,250,255,3,2};
     //updating each centroid
     double temp=0;
     for(int l=0;l<CENTROIDS;l++){
-	if((temp=update_centroid_location(centroids[l]->location, l, NUMPIX))>largest_change)
+	if((temp=update_centroid_location(centroids[l]->location, l, NUMPIX, pixel_data, DIMENSIONS))>largest_change)
 	    largest_change=temp;
-	printf("Centroid %i moved: %f\n",l, temp);
     }
+    printf("avg_dist: %f\n", get_avg_dist(NUMPIX,pixel_data));
     } while(largest_change>0.0001);
-    for(int i=0;i<NUMPIX;i++){
-	printf("Pixel %i's Centroid: %i\n",i,pixel_data[i]->centroid);
-    }
-    printf("Averadge Distance: %f\n",(get_avg_dist(NUMPIX)));
-    free_all(pixel_data, NUMPIX);
+    //free_all(pixel_data, NUMPIX);
     free_all(centroids, CENTROIDS);
+    return pixel_data;
 }
-double get_avg_dist(int numpix){
+double get_avg_dist(int numpix, struct pixel ** pixel_data){
     double avg=0;
     for(int i=0;i<numpix;i++){
 	avg+=pixel_data[i]->lowest_dist;
@@ -84,7 +57,7 @@ double get_avg_dist(int numpix){
 }
 //returns difference between the centroid
 //useful to know how a centroid is changing
-double update_centroid_location(double *centroid, int centroid_num, int numpix){
+double update_centroid_location(double *centroid, int centroid_num, int numpix, struct pixel ** pixel_data, int DIMENSIONS){
     double old_cent[DIMENSIONS];
     memcpy(old_cent,centroid,DIMENSIONS*sizeof(double));
     double *sum=malloc(DIMENSIONS*sizeof(double));
@@ -107,9 +80,9 @@ double update_centroid_location(double *centroid, int centroid_num, int numpix){
     }
     memcpy(centroid,sum,DIMENSIONS*sizeof(double));
     free(sum);
-    return get_diff(centroid, old_cent);
+    return get_diff(centroid, old_cent, DIMENSIONS);
 }
-double get_diff(double * cent, double * old_cent){
+double get_diff(double * cent, double * old_cent, int DIMENSIONS){
     double diff=0;
     for(int i=0;i<DIMENSIONS; i++){
 	diff+=sqrt(pow(cent[i]-old_cent[i],2));
@@ -118,8 +91,7 @@ double get_diff(double * cent, double * old_cent){
 }
 void free_all(struct pixel ** arr, int count){
     for(int i=0;i<count;i++){
-	//comment next line for demo
-	//free(arr[i]->location);
+	free(arr[i]->location);
 	free(arr[i]);
     }
 }
@@ -134,7 +106,7 @@ struct pixel * add_pixel(double * location){
     new->location=location;
     return new;
 }
-void print_pixel(struct pixel * p){
+void print_pixel(struct pixel * p, int DIMENSIONS){
     printf("Location: [");
     for(int i=0;i<DIMENSIONS;i++){
 	printf("%f,", p->location[i]);
@@ -149,7 +121,7 @@ void normalize_vals(double  *pixel_data, int size, int * min_vals, int * max_val
 double normalize_val(double value, double max_vals, double min_vals){
     return (value-min_vals)/(max_vals-min_vals);
 }
-double * place_k(int * min_vals, int * max_vals){
+double * place_k(int * min_vals, int * max_vals, int DIMENSIONS){
     double * tmp= malloc(DIMENSIONS*sizeof(double));
     if(!tmp){
     perror("Could not allocate space");
@@ -160,7 +132,7 @@ double * place_k(int * min_vals, int * max_vals){
     }
     return tmp;
 }
-double get_distance(double * centroid_location, double * pixel_data){
+double get_distance(double * centroid_location, double * pixel_data, int DIMENSIONS){
     double total=0;
     for(int i=0; i<DIMENSIONS;i++){
 	total+=pow(centroid_location[i]-pixel_data[i],2);
--- k-means.h
+++ k-means.h
@@ -1,20 +1,22 @@
 #ifndef k_means
 #define k_means
-#define NUMPIX (max_vals[3]*max_vals[4])
+#define NUMPIX (max_vals[0]*max_vals[1])
 struct pixel{
 int pixel_num;
 double lowest_dist;
 int centroid;
 double * location;
 };
-double get_avg_dist(int numpix);
-double get_diff(double * cent, double * old_cent);
-double update_centroid_location(double *centroid, int centroid_num, int numpix);
+struct pixel ** pixel_data;
+struct pixel ** kmeans(int *min_vals, int * max_vals, int CENTROIDS, int DIMENSIONS, double ** pixel_raw_data);
+double get_avg_dist(int numpix, struct pixel ** pixel_data);
+double get_diff(double * cent, double * old_cent, int DIMENSIONS);
+double update_centroid_location(double *centroid, int centroid_num, int numpix, struct pixel ** pixel_data, int DIMENSIONS);
 void free_all(struct pixel ** arr, int count);
 struct pixel * add_pixel(double * location);
 void normalize_vals(double  *pixel_data, int size, int * min_vals, int * max_vals);
-void print_pixel(struct pixel * p);
+void print_pixel(struct pixel * p, int DIMENSIONS);
 double normalize_val(double value, double max_vals, double min_vals);
-double * place_k(int * min_vals, int * max_vals);
-double get_distance(double * centroid_location, double * pixel_data);
+double * place_k(int * min_vals, int * max_vals, int DIMENSIONS);
+double get_distance(double * centroid_location, double * pixel_data, int DIMENSIONS);
 #endif
