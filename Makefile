48d3c6e38191ffde16dc8f9a914bc06afc2d3a6b
Liu Jiachen <jliu2@clyde>
2018-05-15 21:04:34 -0400

    everything compiled

--- Makefile
+++ Makefile
@@ -1,5 +1,16 @@
-all: k-means
-k-means: k-means.c k-means.h image-output.h
-	gccx -o -lm -ljepg image_segmentation image_segmentation.c image-input.c image-output.c k-means.c
+all: image_segmentation
+image-output.o: image-output.c image-output.h
+	gccx image-output.c -c 
+
+k-means.o: k-means.c k-means.h
+	gccx k-means.c -c
+
+image-input.o: image-input.c image-input.h
+	gccx image-input.c -c 
+
+
+image_segmentation:  k-means.h image-output.h image-input.h image-input.c image-output.c k-means.c image_segmentation.c image-output.o k-means.o image-input.o
+	gccx image_segmentation.c image-input.o image-output.o k-means.o -o image_segmentation -ljpeg -lm
+
 clean:
-	rm k-means
+	rm image_segmentation image-output.o image-input.o k-means.o
--- image_segmentation.c
+++ image_segmentation.c
@@ -1,6 +1,13 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include "k-means.h"
 #include "image-output.h"
+#include "image-input.h"
 #include <ctype.h>
+#include <string.h>
+#include <strings.h>
+
 
 void free_array(int * arr){
     for (int i=0; i<=4; i++){
@@ -19,15 +26,15 @@ int main(int argc, char **argv){
 	exit(1);
     }
     if (argc ==4){
-	output_filename = agrv[3];
+	output_filename = argv[3];
     }
     char *filename = argv[1]; 
-    int k = argv[2]-'0';
-    int * min_array[5];
-    int * max_array[5];
-    double ** pixel_raw_data= read_JPEG_file(filename, &min_array, &max_array);
+    int k = *argv[2]-'0';
+    int min_array[5];
+    int max_array[5];
+    double ** pixel_raw_data= read_JPEG_file(filename, min_array, max_array);
     int * cluster = kmeans(min_array, max_array, k, 5, pixel_raw_data);
-    output_image(cluster, max[1], max[0], k, output_filename) ;    
+    output_image(cluster, max_array[1], max_array[0], k, output_filename) ;    
     free_array(min_array);
     free_array(max_array);
     return 0;
