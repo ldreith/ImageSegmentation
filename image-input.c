fe254dc1e90d99abe7fa208e8823bdd3ee83e773
Lars Dreith <ldreith@oberlin.edu>
2018-05-15 20:29:45 -0400

    fixing errors in image-input

--- image-input.c
+++ image-input.c
@@ -26,7 +26,6 @@ double ** read_JPEG_file (char * filename, int * min_array, int * max_array)
 {
     unsigned long x, y;
     unsigned long data_size;	// length of file
-    unsigned char * rowptf[1];	// pointer to an array
     unsigned char * jdata;	// data for the image
     struct jpeg_decompress_struct info;	// for our jpeg info
     struct jpeg_error_mgr err;		// the error handler
@@ -67,8 +66,8 @@ double ** read_JPEG_file (char * filename, int * min_array, int * max_array)
     x = info.output_width;
     y = info.output_height;
     data_size = x * y * 3;
-    max_array[0] = x;
-    max_array[1] = y;
+    max_array[0] = (int) x;
+    max_array[1] = (int) y;
 
     // creating array of appropriate size
     double ** array=malloc(sizeof(double)*(x*y));
@@ -76,7 +75,7 @@ double ** read_JPEG_file (char * filename, int * min_array, int * max_array)
 	perror("Could not allocate space.");
 	exit(4);
     }
-    for(int i=0;i<5;i++){
+    for(int i=0;i<=(int) (y * x);i++){
 	array[i]=malloc(sizeof(double)*5);
 	if(!array[i]){
 	    perror("Could not allocate space");
@@ -97,19 +96,17 @@ double ** read_JPEG_file (char * filename, int * min_array, int * max_array)
     int count = 0;
     while( info.output_scanline < info.output_height )
     {
-	jpeg_read_scanlines(&info, rowptf, 1);
+	jpeg_read_scanlines(&info, pJpegBuffer, 1); 
 	if( count == 0 ){
 	    r = pJpegBuffer[0][info.output_components * 0];
 	    g = pJpegBuffer[0][info.output_components * 1];
 	    b = pJpegBuffer[0][info.output_components * 2];
-	    array[count][2] = r; array[count][3] = g; array[count][4] = b;
 	    r_max = r; r_min = r;
 	    g_max = g; g_min = g;
 	    b_max = b; b_min = b;
-	    count++;
-	} else{
+	}
 	    for (int i = 0; i < (int) x; i++){
-		array[count][0] = i; array[count][2] = row;
+		array[count][0] = i; array[count][1] = row;
 		r = pJpegBuffer[0][info.output_components * i];
 		g = pJpegBuffer[0][info.output_components * i + 1];
 		b = pJpegBuffer[0][info.output_components * i + 2];
@@ -121,16 +118,15 @@ double ** read_JPEG_file (char * filename, int * min_array, int * max_array)
 		if( b > b_max ){ b_max = b; }
 		if( b < b_min ){ b_min = b; }
 		count++;
-	    }
 	}
 
 	row++;
     }
 
     // setting info in max and min arrays
-    max_array[2] = r_max; min_array[2] = r_min;
-    max_array[3] = g_max; min_array[3] = g_min;
-    max_array[4] = b_max; min_array[4] = b_min;
+    max_array[2] = (int) r_max; min_array[2] = (int) r_min;
+    max_array[3] = (int) g_max; min_array[3] = (int) g_min;
+    max_array[4] = (int) b_max; min_array[4] = (int) b_min;
 
     jpeg_finish_decompress(&info); // finish decompressing
     
